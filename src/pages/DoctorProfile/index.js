a469d6bab3ff7a1abf6ee4d4d38abe48d60b12bc
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-07-14 13:54:34 +0700

    send data parameters to doctor profile, and display doctro profile become dynamic

--- src/pages/DoctorProfile/index.js
+++ src/pages/DoctorProfile/index.js
@@ -5,22 +5,23 @@ import {Header, Profile, ProfileItem, Button, Gap} from '../../components';
 import {DummyDoctor4} from '../../assets';
 import {colors} from '../../utils';
 
-const DoctorProfile = ({navigation}) => {
+const DoctorProfile = ({navigation, route}) => {
+  const dataDoctor = route.params;
   return (
     <View style={styles.page}>
       <Header title="Doctor Profile" onPress={() => navigation.goBack()} />
       <Profile
-        name="Alifah Nurdianti"
-        desc="Doctor Anak"
-        avatar={DummyDoctor4}
+        name={dataDoctor.data.fullName}
+        desc={dataDoctor.data.profession}
+        avatar={{uri: dataDoctor.data.photo}}
       />
       <Gap height={10} />
-      <ProfileItem label="Alumnus" value="Universitas Indonesia" />
+      <ProfileItem label="Alumnus" value={dataDoctor.data.university} />
       <ProfileItem
         label="Tempat Praktik"
-        value="Rumah Sakit Umum Daerah Bekasi"
+        value={dataDoctor.data.hospital_address}
       />
-      <ProfileItem label="No. STR" value="STR-0809900982772" />
+      <ProfileItem label="No. STR" value={dataDoctor.data.str_number} />
       <View style={styles.action}>
         <Button
           title="Start Consultation"
